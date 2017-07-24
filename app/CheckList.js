892fee42ffa11db3a901e5c39dad65cc8ba432e2
leo <fffweb@hotmail.com>
2017-07-25 06:20:17 +0800

    fix task not found error

--- app/CheckList.js
+++ app/CheckList.js
@@ -11,10 +11,10 @@ class CheckList extends Component {
     let tasks = this.props.tasks.map((task,taskIndex) => (
       <li key={task.id} className="checklist__task">
         <input type="checkbox" defaultChecked={task.done} 
-        onChange={this.props.taskCallbacks.toggle.bind(null,this.props.cardid,task.id,taskIndex)}/>
+        onChange={this.props.taskCallbacks.toggle.bind(null,this.props.cardId,task.id,taskIndex)}/>
         {task.name}
         <a href="#" className="checklist__task--remove" 
-        onClick={this.props.taskCallbacks.delete.bind(null,this.props.cardid,task.id,taskIndex)}
+        onClick={this.props.taskCallbacks.delete.bind(null,this.props.cardId,task.id,taskIndex)}
         
         />
       </li>
